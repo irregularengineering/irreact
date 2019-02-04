042107f67054bbbe5f6878ab7e60d74fcc8bc059
reed-only <mattreed79@gmail.com>
2019-02-03 18:41:55 -0800

    Router (#3)

---
+++ src/components/frameworks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Frameworks extends Component {
+  render() {
+    return (
+        <h2>Frameworks</h2>
+    );
+  }
+}
+
+export default Frameworks;
---
+++ src/components/languages.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Languages extends Component {
+  render() {
+    return (
+        <h2>Languages</h2>
+    );
+  }
+}
+
+export default Languages;
